79cf085c3185fad24e8c1121b21a821599b656c6
ChrisTiller <ctiller256@gmail.com>
2017-03-28 14:50:00 -0400

    remove search bar

--- tables.js
+++ tables.js
@@ -1,9 +1,9 @@
 var removeData = [
-  
-   
+
+
 
 ];
-    
+
 
 $(document).ready(function() {
   var table = $('#addTable').DataTable( {
@@ -12,9 +12,10 @@ $(document).ready(function() {
             "data": null,
             "defaultContent": "<button>MODIFY</button>"
         } ],
-        "ajax": 'ajax.txt'
+        "ajax": 'ajax.txt',
+        "searching": false
     } );
-  
+
  var table2 =  $('#removeTable').DataTable( {
       "scrollX": true,
       "scrollY":        "575px",
@@ -39,39 +40,39 @@ $(document).ready(function() {
             { title: "INSTRUCTOR" }
         ]
     } );
-  
-  
+
+
   // Setup - add a text input to each footer cell
     $('#addTable tfoot th').each( function () {
         var title = $(this).text();
         $(this).html( '<input type="text" placeholder="Search" />' );
     } );
- 
+
    $('#addTable tbody').on( 'click', 'button', function () {
- 
+
     $('#addTable').dataTable().fnDeleteRow($(this).closest('tr')[0]);
-$('#removeTable').dataTable().fnAddData($(this).closest('tr'));  
-    
-    
-    
-    
+$('#removeTable').dataTable().fnAddData($(this).closest('tr'));
+
+
+
+
     } );
-  
+
   $('#removeTable tbody').on( 'click', 'button', function () {
- 
+
     $('#removeTable').dataTable().fnDeleteRow($(this).closest('tr')[0]);
-  $('#addTable').dataTable().fnAddData($(this).closest('tr'));  
-    
-    
-    
-    
+  $('#addTable').dataTable().fnAddData($(this).closest('tr'));
+
+
+
+
     } );
-   
- 
+
+
     // Apply the search
     table.columns().every( function () {
         var that = this;
- 
+
         $( 'input', this.footer() ).on( 'keyup change', function () {
             if ( that.search() !== this.value ) {
                 that
